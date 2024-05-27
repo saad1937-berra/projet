a7f6a1188ae0ec269a694f25279dd3156de2e890
saad1937-berra <saadberra11@gmail.com>
2024-05-27 20:39:28 +0100

    First project

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("This is the console");
+
+console.log("This is the second message");
+
+alert("this is alert function");
+
+var a = 10
+var b = 5
+console.log(parseFloat(a+b));
+console.log(parseFloat(a-b));
+console.log(parseFloat(a*b));
+console.log(parseFloat(a/b));
